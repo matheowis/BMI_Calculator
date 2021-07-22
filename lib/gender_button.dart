e734552b5e26c75f7a8b6b8ae8abeaccf83237ec
Wiśniowski Mateusz <mateusz.wisniowski@wisniowski.pl>
2021-07-22 15:02:02 +0200

    My Stuff

---
+++ lib/gender_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class GenderButton extends StatelessWidget {
+  const GenderButton({
+    Key? key,
+    required this.onPressed,
+    this.title = "",
+    this.icon,
+  }) : super(key: key);
+
+  final VoidCallback? onPressed;
+  final String title;
+  final IconData? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 80.0,
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Color(0xFF8D8E98),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
